c4b0791290f9463b7bd9b2b19064b9247d122e2c
Mikhail Belin <belinma@yandex.ru>
2024-07-23 17:24:11 +0300

    v3.1: sort_by_date

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from typing import Any, Callable, Iterable
 import sys
+from typing import Any, Callable, Iterable
 
 from src.masks import get_mask_account, get_mask_card_number
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state
 
 
 def functest(func: Callable, test_arr: Iterable[Any]) -> None:
@@ -62,24 +62,33 @@ def main() -> None:
 
     if len(sys.argv) == 1:
         processing_test = [
-            [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
         ]
 
         for i in processing_test:
-            print("data, ", end='')
+            print("data, ", end="")
             print_iterable(i)
             print()
 
-            print("(*) filter_by_state(data) --> ", end='')
+            print("(*) filter_by_state(data) --> ", end="")
             print_iterable(filter_by_state(i))
             print()
 
-            print("(*) filter_by_state(data, 'CANCELED') --> ", end='')
-            print_iterable(filter_by_state(i, 'CANCELED'))
+            print("(*) filter_by_state(data, 'CANCELED') --> ", end="")
+            print_iterable(filter_by_state(i, "CANCELED"))
             print()
+
+            print("(*) sort_by_date(data) --> ", end="")
+            print_iterable(sort_by_date(i))
+            print()
+
+            print("(*) sort_by_date(data, reverse=True) --> ", end="")
+            print_iterable(sort_by_date(i, reverse=True))
 
 
 if __name__ == "__main__":
--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,9 @@
-from typing import Iterable
+from typing import Callable, Iterable
+
+from src.widget import get_date
 
 
-def filter_by_state(data: Iterable[dict], state='EXECUTED') -> Iterable[dict] | None:
+def filter_by_state(data: Iterable[dict], state: str = "EXECUTED") -> Iterable[dict] | None:
     """Returns selected data as list of dictionaries
     which 'state' option is equal to state string (default: 'EXECUTED'),
     returns None if input data is not a list or state is not a string"""
@@ -11,6 +13,16 @@ def filter_by_state(data: Iterable[dict], state='EXECUTED') -> Iterable[dict] | 
     for elem in data:
         if not isinstance(elem, dict):
             continue
-        if 'state' in elem.keys() and elem['state'] == state:
+        if "state" in elem.keys() and elem["state"] == state:
             to_return.append(elem)
     return to_return
+
+
+def sort_by_date(data: Iterable[dict], reverse: bool = False) -> Iterable[dict] | None:
+    if not isinstance(data, list):
+        return None
+    # calling get_date() here is for right type of date and time checking
+    dict_date: Callable[[dict], str | None] = lambda elem: (
+        elem["date"] if "date" in elem.keys() and not get_date(elem["date"]) is None else None
+    )
+    return sorted(data, key=dict_date, reverse=reverse)
